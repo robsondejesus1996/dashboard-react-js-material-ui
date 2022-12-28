64256e95235001069b5f05943d8c140e79bf3c02
robson <robsondejesus1996@hotmail.com>
2022-12-28 15:35:57 -0300

    Configuração dos temas e troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import { ThemeProvider } from "@mui/material";
+//import { ThemeProvider } from "@mui/material";
+
+
 import { BrowserRouter } from "react-router-dom";
-
-
-import {LightTheme} from './shared/themes'
-
 import { AppRouter } from "./routes";
+import { AppThemeProvider } from "./shared/contexts/ThemeContext";
+
+//import {DarkTheme, LightTheme} from './shared/themes'
+
+
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider >
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
-    </ThemeProvider>
+      </AppThemeProvider>
   );
 }
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@mui/material';
 import { Routes, Route, Navigate} from 'react-router-dom'
+import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRouter = () =>{
+    const { toggleTheme } = useAppThemeContext();
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<p> <Button variant='contained' color='primary'>Olá mundo</Button> </p>}/>
+            <Route path="/pagina-inicial" element={<p> <Button variant='contained' color='primary' onClick={toggleTheme }>Togle Theme</Button> </p>}/>
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
